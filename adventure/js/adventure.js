e514eb85ac11c3b5ca79b96d1589320ce4976467
Arthur Pachachura <pachachura.arthur@gmail.com>
2014-05-29 12:48:59 -0500

    Added Music

--- adventure/js/adventure.js
+++ adventure/js/adventure.js
@@ -22,6 +22,7 @@ function firstLoad()
   
   //Animate scroll down
   $(anim_scrolldown);
+ play_sound('../files/Dschinghis Khan - Dschinghis Khan.mp3');
   
 }
 
@@ -41,6 +42,30 @@ function anim_scrolldown(){
   $('.scrolldown').animate({
     opacity: 1,
   }, 1000, anim_scrolldown);
+}
+
+function html5_audio(){
+	var a = document.createElement('audio');
+	return !!(a.canPlayType && a.canPlayType('audio/mpeg;').replace(/no/, ''));
+}
+ 
+var play_html5_audio = false;
+if(html5_audio()) play_html5_audio = true;
+ 
+function play_sound(url){
+	if(play_html5_audio){
+		var snd = new Audio(url);
+		snd.load();
+		snd.play();
+	}else{
+		$("#sound").remove();
+		var sound = $("<embed id='sound' type='audio/mpeg' />");
+		sound.attr('src', url);
+		sound.attr('loop', false);
+		sound.attr('hidden', true);
+		sound.attr('autostart', true);
+		$('body').append(sound);
+	}
 }
 
 /**
